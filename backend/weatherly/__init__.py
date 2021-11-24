ce268c2a6f2d739a65c256c06b646ce04392a3e5
Rafael Avilés <rafa.vfierro@outlook.com>
2021-11-24 13:30:14 -0800

    feat(backend): add forecast blueprint to app factory

--- backend/weatherly/__init__.py
+++ backend/weatherly/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from . import forecast 
 
 def create_app(config_filename="flask.cfg"):
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_pyfile(config_filename)
+    app.register_blueprint(forecast.bp)
     
     return app
