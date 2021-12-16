218f82ce4c1be6601f3bbe1cecc50257a970059d
Rafael Avilés <rafa.vfierro@outlook.com>
2021-12-16 15:00:22 -0800

    feat(frontend): add icon script and icon logic to Conditions components

--- client/src/components/Conditions/ConditionCard/ConditionCard.tsx
+++ client/src/components/Conditions/ConditionCard/ConditionCard.tsx
@@ -1,13 +1,21 @@
 import { ReactElement } from "react";
+import "./ConditionCard.css";
 
-function ConditionCard(): ReactElement {
+interface Props {
+  name: string;
+  value?: string;
+  icon: string;
+}
+
+function ConditionCard({ name, value, icon }: Props): ReactElement {
   return (
     <div className="condition-card">
-      <figure>
-        <img src="" alt="" />
-        <figcaption>Sunrise</figcaption>
+      <figure className="condition-icon">
+        {/* the iconify script turns this span into an svg*/}
+        <span className="iconify" data-icon={icon}></span>
+        <figcaption>{name}</figcaption>
       </figure>
-      <p className="condition-value">06:16 AM</p>
+      <p className="condition-value">{value}</p>
     </div>
   );
 }
--- client/src/components/Conditions/Conditions.tsx
+++ client/src/components/Conditions/Conditions.tsx
@@ -1,12 +1,77 @@
-import { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 import { ConditionCard } from "./ConditionCard/ConditionCard";
+import "./Conditions.css";
+
+function unixToDate(
+  unixTimestamp?: number,
+  timeZone: string = "America/Los_Angeles"
+): string {
+  if (unixTimestamp) {
+    const miliseconds = unixTimestamp * 1000;
+    const date = new Date(miliseconds);
+
+    return date.toLocaleTimeString("en-GB", { timeZone });
+  }
+
+  return "N/A";
+}
 
 function Conditions(): ReactElement {
-  return (
-    <section className="conditions-container">
-      <ConditionCard />
-    </section>
-  );
+  const { forecast } = useContext(GlobalContext);
+  const current = forecast?.imperial.current;
+  const daily = forecast?.imperial.daily[0];
+  const timeZone = forecast?.imperial.timezone;
+
+  const conditions = [
+    {
+      id: 1,
+      value: `${unixToDate(current?.sunrise, timeZone)} AM`,
+      name: "Sunrise",
+      icon: "wi:sunrise",
+    },
+    {
+      id: 2,
+      value: `${unixToDate(current?.sunset, timeZone)} PM`,
+      name: "Sunset",
+      icon: "wi:sunset",
+    },
+    {
+      id: 3,
+      value: current?.wind_speed + " mph",
+      name: "Wind Speed",
+      icon: "wi:strong-wind",
+    },
+    {
+      id: 4,
+      value: current?.feels_like + " °F",
+      name: "Feels Like",
+      icon: "wi:thermometer",
+    },
+    {
+      id: 5,
+      value: current?.humidity + "%",
+      name: "Humidity",
+      icon: "wi:humidity",
+    },
+    {
+      id: 6,
+      value: daily?.pop + "%",
+      name: "Precipitation",
+      icon: "wi:raindrops",
+    },
+  ];
+
+  const Cards = conditions.map((item) => (
+    <ConditionCard
+      key={item.id}
+      name={item.name}
+      value={item.value?.toString()}
+      icon={item.icon}
+    />
+  ));
+
+  return <section className="conditions-container">{Cards}</section>;
 }
 
 export { Conditions };
