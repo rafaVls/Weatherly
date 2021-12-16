10c99bb3b9ba22b907a61d10f820cb02b20fa351
Rafael Avilés <rafa.vfierro@outlook.com>
2021-12-16 14:58:16 -0800

    feat(frontend): add state (context) to CurrentWeather and Temperatures components

--- client/src/components/CurrentWeather/CurrentWeather.tsx
+++ client/src/components/CurrentWeather/CurrentWeather.tsx
@@ -1,12 +1,25 @@
-import { ReactElement } from "react";
+import { ReactElement, useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 import { Temperatures } from "./Temperatures/Temperatures";
+import "./CurrentWeather.css";
+
+function getDayIcon(icon: string): string {
+  return `http://openweathermap.org/img/wn/${icon}@4x.png`;
+}
 
 function CurrentWeather(): ReactElement {
+  const { forecast } = useContext(GlobalContext);
+  const weather = forecast?.metric.current.weather[0];
+
   return (
     <section className="current-weather">
       <figure>
-        <img src="" alt="" />
-        <figcaption>Rainy</figcaption>
+        <img
+          src={weather && getDayIcon(weather.icon)}
+          alt={weather?.description}
+          title={weather?.main}
+        />
+        <figcaption>{weather?.main}</figcaption>
       </figure>
 
       <Temperatures />
--- client/src/components/CurrentWeather/Temperatures/Temperatures.tsx
+++ client/src/components/CurrentWeather/Temperatures/Temperatures.tsx
@@ -1,14 +1,39 @@
-import { ReactElement } from "react";
+import { ReactElement, useContext, useEffect, useState } from "react";
+import { GlobalContext } from "../../../context/GlobalState";
+import { Temp } from "../../../common/types/Forecast";
+import "./Temperatures.css";
 
 function Temperatures(): ReactElement {
+  const { forecast } = useContext(GlobalContext);
+  const [temperatures, setTemperatures] = useState({
+    current: 0,
+    max: 0,
+    min: 0,
+  });
+
+  useEffect(() => {
+    if (forecast) {
+      const currentTemp = forecast.imperial.current.temp as number;
+      const dailyTemp = forecast.imperial.daily[0].temp as Temp;
+
+      setTemperatures({
+        current: +currentTemp.toFixed(0),
+        max: +dailyTemp.max.toFixed(0),
+        min: +dailyTemp.min.toFixed(0),
+      });
+    }
+  }, [forecast]);
+
   return (
     <div className="temperatures">
       <p className="current">
-        78<sup>°F</sup>
+        {temperatures.current}
+        <sup>°F</sup>
       </p>
       <div className="min-max">
-        <p className="max">Max 82</p>
-        <p className="min">Min 74</p>
+        <p className="max">Max {temperatures.max}</p>
+        <div className="separator"></div>
+        <p className="min">Min {temperatures.min}</p>
       </div>
     </div>
   );
